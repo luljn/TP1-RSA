acb13ebdb856944f099ea6e0df5e024e8d4bf183
luljn <mbohlulajonathan4@gmail.com>
2024-05-22 12:24:56 +0200

    Encryption and decryption of the message with sha256

--- MBECK_Lula_util.py
+++ MBECK_Lula_util.py
@@ -2,6 +2,7 @@ import hashlib
 import binascii
 
 def home_mod_expnoent(x, y, n):
+    # x^y % n
     resultat = 1
     x = x % n
     while y > 0:
@@ -60,23 +61,38 @@ def mot10char(): #entrer le secret
     while (len(secret)>11):
         secret=input("c'est beaucoup trop long, 10 caractères S.V.P : ")
     return(secret)
+
+
+
+def CRT(xi, xj, d, c, n):
+    p = max(xi, xj)
+    q = min(xi, xj)
+    inv_q = home_ext_euclide(p, q) # q^-1
+    dq = home_mod_expnoent(d, 1, (q-1)) #dq
+    dp = home_mod_expnoent(d, 1, (p-1)) #dp
+    mq = home_mod_expnoent(c, dq, q)
+    mp = home_mod_expnoent(c, dp, p)
+    h = home_mod_expnoent((((mp - mq)) * inv_q), 1, p)
+    m = home_mod_expnoent((mq + h*q), 1, n)
+    
+    return m
     
 
 
 def main():
     #voici les éléments de la clé d'Alice
-    x1a=2010942103422233250095259520183 #p
-    x2a=3503815992030544427564583819137 #q
+    x1a=7629726037683340809053781301491052450903774696953640341584051231729511370379166360126409803221117123 #p
+    x2a=8363655490175461917767868789427975623690693529579788555091444824311849262262236116059668047613888361 #q
     na=x1a*x2a  #n
     phia=((x1a-1)*(x2a-1))//home_pgcd(x1a-1,x2a-1)
-    ea=83 #exposant public
+    ea=65537 #exposant public
     da=home_ext_euclide(phia,ea) #exposant privé
     #voici les éléments de la clé de bob
-    x1b=9434659759111223227678316435911 #p
-    x2b=8842546075387759637728590482297 #q
+    x1b=1099574839562278306064474283355448461332374062045181832660087732634718557396781568567210505711041857 #p
+    x2b=4979882905343183355397178910495211912545039962516000340728727490133703128519702970841414312070613091 #q
     nb=x1b*x2b # n
     phib=((x1b-1)*(x2b-1))//home_pgcd(x1b-1,x2b-1)
-    eb=89 # exposants public
+    eb=65537 # exposants public
     db=home_ext_euclide(phib,eb) #exposant privé
 
 
@@ -105,7 +121,7 @@ def main():
     print(chif)
     print("*******************************************************************")
     print("On utilise la fonction de hashage MD5 pour obtenir le hash du message",secret)
-    Bhachis0=hashlib.md5(secret.encode(encoding='UTF-8',errors='strict')).digest() #MD5 du message
+    Bhachis0=hashlib.sha256(secret.encode(encoding='UTF-8')).digest()#.md5(secret.encode(encoding='UTF-8',errors='strict')).digest() #MD5 du message
     print("voici le hash en nombre décimal ")
     Bhachis1=binascii.b2a_uu(Bhachis0)
     Bhachis2=Bhachis1.decode() #en string
@@ -120,14 +136,14 @@ def main():
     x=input("appuyer sur entrer")
     print("*******************************************************************")
     print("Alice déchiffre le message chiffré \n",chif,"\nce qui donne ")
-    dechif=home_int_to_string(home_mod_expnoent(chif, da, na))
+    dechif=home_int_to_string(CRT(x1a, x2a, da, chif, na))#()home_mod_expnoent(chif, da, na)
     print(dechif)
     print("*******************************************************************")
     print("Alice déchiffre la signature de Bob \n",signe,"\n ce qui donne  en décimal")
-    designe=home_mod_expnoent(signe, eb, nb)
+    designe=CRT(x1b, x2b, eb, signe, nb)#home_mod_expnoent(signe, eb, nb)
     print(designe)
     print("Alice vérifie si elle obtient la même chose avec le hash de ",dechif)
-    Ahachis0=hashlib.md5(dechif.encode(encoding='UTF-8',errors='strict')).digest()
+    Ahachis0=hashlib.sha256(dechif.encode(encoding='UTF-8')).digest()#.md5(dechif.encode(encoding='UTF-8',errors='strict')).digest()
     Ahachis1=binascii.b2a_uu(Ahachis0)
     Ahachis2=Ahachis1.decode()
     Ahachis3=home_string_to_int(Ahachis2)
